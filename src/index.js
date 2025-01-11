faad2bcd7a9103ea2ffe4ba6c750955c80651d91
kristban <kristinbannasch@hotmail.com>
2025-01-11 23:12:01 +0000

    added the view for selected city in JS

--- src/index.js
+++ src/index.js
@@ -1,33 +1,61 @@
 function updateLosAngelesTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("DD. MMMM YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format("HH:mm:ss");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("DD. MMMM YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format("HH:mm:ss");
+  }
 }
 
 function updateDublinTime() {
   let dublinElement = document.querySelector("#dublin");
-  let dublinDateElement = dublinElement.querySelector(".date");
-  let dublinTimeElement = dublinElement.querySelector(".time");
-  let dublinTime = moment().tz("Europe/Dublin");
+  if (dublinElement) {
+    let dublinDateElement = dublinElement.querySelector(".date");
+    let dublinTimeElement = dublinElement.querySelector(".time");
+    let dublinTime = moment().tz("Europe/Dublin");
 
-  dublinDateElement.innerHTML = dublinTime.format("DD. MMMM YYYY");
-  dublinTimeElement.innerHTML = dublinTime.format("HH:mm:ss");
+    dublinDateElement.innerHTML = dublinTime.format("DD. MMMM YYYY");
+    dublinTimeElement.innerHTML = dublinTime.format("HH:mm:ss");
+  }
 }
 
 function updateAucklandTime() {
   let aucklandElement = document.querySelector("#auckland");
-  let aucklandDateElement = aucklandElement.querySelector(".date");
-  let aucklandTimeElement = aucklandElement.querySelector(".time");
-  let aucklandTime = moment().tz("Pacific/Auckland");
+  if (aucklandElement) {
+    let aucklandDateElement = aucklandElement.querySelector(".date");
+    let aucklandTimeElement = aucklandElement.querySelector(".time");
+    let aucklandTime = moment().tz("Pacific/Auckland");
 
-  aucklandDateElement.innerHTML = aucklandTime.format("DD. MMMM YYYY");
-  aucklandTimeElement.innerHTML = aucklandTime.format("HH:mm:ss");
+    aucklandDateElement.innerHTML = aucklandTime.format("DD. MMMM YYYY");
+    aucklandTimeElement.innerHTML = aucklandTime.format("HH:mm:ss");
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
 
 setInterval(updateLosAngelesTime, 1000);
 setInterval(updateDublinTime, 1000);
 setInterval(updateAucklandTime, 1000);
+
+
+let citiesSelectElement = document.querySelector("#selection");
+citiesSelectElement.addEventListener("change", updateCity);
